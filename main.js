b09d313d7310aad9d243080681fd4f16d54bdfb9
Nicholas Mutale <nicomutale@gmail.com>
2022-02-16 17:31:08 +0200

    fix linter error update main js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let storedBooks = [];
-const list = document.querySelector(".bookList");
-const form = document.querySelector("form");
-const title = document.querySelector("#bookTitle");
-const author = document.querySelector("#bookAuthor");
+const list = document.querySelector('.bookList');
+const form = document.querySelector('form');
+const title = document.querySelector('#bookTitle');
+const author = document.querySelector('#bookAuthor');
 
 function removebook(bookinfo, index) {
   const bookData2 = document.getElementById(index);
@@ -12,20 +12,20 @@ function removebook(bookinfo, index) {
     (item) => item.author !== aut && item.title !== tit
   );
 
-  localStorage.setItem("bookCollection", JSON.stringify(storedBooks));
+  localStorage.setItem('bookCollection', JSON.stringify(storedBooks));
   list.removeChild(bookData2);
 }
 
 function addBook(bookinfo, index) {
-  const bookData = document.createElement("div");
-  bookData.classList.add("bookData");
+  const bookData = document.createElement('div');
+  bookData.classList.add('bookData');
   bookData.id = index;
 
-  const removeButton = document.createElement("button");
-  removeButton.classList.add("remove-btn");
-  removeButton.innerText = "Remove";
+  const removeButton = document.createElement('button');
+  removeButton.classList.add('remove-btn');
+  removeButton.innerText = 'Remove';
 
-  const horizontalLine = document.createElement("hr");
+  const horizontalLine = document.createElement('hr');
 
   bookData.innerHTML = `<p class='bookTitle'>${bookinfo.title}</p> 
     <p class='bookAuthor'>${bookinfo.author}</p> `;
@@ -44,27 +44,27 @@ function addBookItems(item) {
     author: author.value,
   });
 
-  localStorage.setItem("bookCollection", JSON.stringify(storedBooks));
-  title.value = "";
-  author.value = "";
+  localStorage.setItem('bookCollection', JSON.stringify(storedBooks));
+  title.value = '';
+  author.value = '';
   addBook(item, storedBooks.length - 1);
 }
 
 function updateUI() {
-  if (localStorage.getItem("bookCollection")) {
-    storedBooks = JSON.parse(localStorage.getItem("bookCollection"));
+  if (localStorage.getItem('bookCollection')) {
+    storedBooks = JSON.parse(localStorage.getItem('bookCollection'));
     storedBooks.forEach((bookinfo, index) => {
       addBook(bookinfo, index);
     });
   } else {
-    localStorage.setItem("bookCollection", "");
+    localStorage.setItem('bookCollection', '');
     storedBooks = [];
   }
 }
 
 updateUI();
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   addBookItems({
     title: title.value,
